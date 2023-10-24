5f9ac028fc1d4b9307cbea0bf5d3428585c72640
Martin Mulyo Syahidin <mulyosyahidin95@gmail.com>
2023-10-24 13:00:37 +0700

    Minggu 2 (24/10/2023) - CRUD Buku

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
@@ -19,3 +20,12 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/abc', [HomeController::class, 'abc']);
 
 Route::get('/mahasiswa/{npm}', [MahasiswaController::class, 'view']);
+
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+
+Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
